3bf9c54dc8286099f8ffa84616d694e9238c6ba3
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-07-15 18:40:23 +0200

    Store WIP: Logged user

--- src/Instances/User.php
+++ src/Instances/User.php
@@ -7,4 +7,28 @@ use LaminimCMS\Generated\GeneratedUser;
 class User extends GeneratedUser
 {
     const COMPONENT = 'lmm-user';
+
+    public function logIn(): static
+    {
+        if ($this->getId() === 0) return $this;
+
+        $_SESSION['lmm-user-id'] = $this->getId();
+        return $this;
+    }
+
+    public function logOut(): static
+    {
+        $_SESSION['lmm-user-id'] = 0;
+        return $this;
+    }
+
+    public static function getLoggedId(): int
+    {
+        return (int)$_SESSION['lmm-user-id'];
+    }
+
+    public static function getLogged(): static
+    {
+        return static::getInstance(static::getLoggedId());
+    }
 }
