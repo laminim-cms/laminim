cb4a2e0cfae251ea069cfd40d4613bf63e8c1c41
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-08-05 18:49:57 +0200

    Update api

--- src/Laminim.php
+++ src/Laminim.php
@@ -72,9 +72,9 @@ class Laminim
         GetRoute::register('/laminim/{_lmm_type}/index', [CmsHttp::class, 'indexItems']);
         GetRoute::register('/laminim/{_lmm_type}/{_lmm_own_type}/{_lmm_field}/index', [CmsHttp::class, 'indexForField']);
         GetRoute::register('/laminim/{_lmm_type}/options', [CmsHttp::class, 'optionItems']);
-        GetRoute::register('/laminim/{_lmm_type}/{id}/read', [CmsHttp::class, 'readItem']);
+        GetRoute::register('/laminim/{_lmm_type}/{_lmm_id}/read', [CmsHttp::class, 'readItem']);
         PostRoute::register('/laminim/{_lmm_type}/create', [CmsHttp::class, 'createItem']);
-        PutRoute::register('/laminim/{_lmm_type}/{_lmm_id}/update', [CmsHttp::class, 'updateItem']);
+        PutRoute::register('/laminim/{_lmm_type}/{id}/update', [CmsHttp::class, 'updateItem']);
         GetRoute::register('/laminim/{_lmm_type}/{_lmm_id}/related/{_lmm_field}', [CmsHttp::class, 'relatedItems']);
         GetRoute::register('/laminim/{_lmm_type}/{_lmm_id}/pivot/{_lmm_field}', [CmsHttp::class, 'pivotItems']);
         GetRoute::register('/laminim/{_lmm_type}/{_lmm_id}/pivot/{_lmm_field}/available', [CmsHttp::class, 'availablePivotItems']);
